8076f3653b1f12088f19d9c81a36bdd473914b0f
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2024-11-04 14:41:46 +0100

    Initial Commit

---
+++ LoadManagement/LoadManagementApp.swift
@@ -0,0 +1,32 @@
+//
+//  LoadManagementApp.swift
+//  LoadManagement
+//
+//  Created by Ásmundur Óskar Ásmundsson on 4.11.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LoadManagementApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
